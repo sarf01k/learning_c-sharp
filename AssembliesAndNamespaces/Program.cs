ea4007f563c61559e0d5885bf41fbae74d453639
sarf01k <isaacsarfo2004@gmail.com>
2024-09-26 12:37:29 +0000

    namespaces

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string text = "First line";
+
+            // string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // File.WriteAllText(Path.Combine(docPath, "WriteFile.txt"), text);
+
+            System.Net.WebClient client = new System.Net.WebClient();
+            string reply = client.DownloadString("https://learn.microsoft.com/en-us/dotnet/api/system.net.webclient.downloadstring?view=net-8.0");
+
+            Console.WriteLine(reply);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UnderstandingScope/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace UnderstandingScope
+{
+    class Program
+    {
+        // access modifiers
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
