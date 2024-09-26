49480faca4e7a3567abe839735cea8d65499b6f0
sarf01k <isaacsarfo2004@gmail.com>
2024-09-26 11:15:42 +0000

    change

--- ObjectLifetime/Program.cs
+++ ObjectLifetime/Program.cs
@@ -6,28 +6,13 @@
         {
             Car myCar = new();
             
-            myCar.Make = "Toyota";
-            myCar.Model = "Supra";
-            myCar.Year = 2020;
-            myCar.Color = "White";
+            // myCar.Make = "Toyota";
+            // myCar.Model = "Supra";
+            // myCar.Year = 2020;
+            // myCar.Color = "White";
 
             Car myOtherCar = myCar;
-
-            Console.WriteLine("{0} {1} {2} {3}",
-                myOtherCar.Make,
-                myOtherCar.Model,
-                myOtherCar.Year,
-                myOtherCar.Color);
-
-            myOtherCar.Model = "Corolla"; // changes model of myCar cos they point to the same address
-
-            Console.WriteLine("{0} {1} {2} {3}",
-                myCar.Make,
-                myCar.Model,
-                myCar.Year,
-                myCar.Color);
-
-            myOtherCar = null;
+            Car myThirdCar = new("Ford", "Escape", 2005, "White");
 
             // Console.WriteLine("{0} {1} {2} {3}",
             //     myOtherCar.Make,
@@ -35,7 +20,15 @@
             //     myOtherCar.Year,
             //     myOtherCar.Color);
 
-            myCar = null;
+            // myOtherCar.Model = "Corolla"; // changes model of myCar cos they point to the same address
+
+            // Console.WriteLine("{0} {1} {2} {3}",
+            //     myCar.Make,
+            //     myCar.Model,
+            //     myCar.Year,
+            //     myCar.Color);
+
+            Car.MyMethod();
 
             Console.ReadLine();
         }
@@ -47,5 +40,29 @@
         public string Model { get; set; }
         public int Year { get; set; }
         public string Color { get; set; }
+
+        // Constructor - allows to execute code when a new instance of a class is created
+        // typically used to initialise values(from a config file, db...) of an object
+
+        // public Car()
+        // {
+        //     this.Make = "Nissan"; // 'this' is optional,("this" instance)
+        // }
+
+        // overloaded constructor
+        // public Car(string make, string model, int year, string color)
+        // {
+        //     Make = make;
+        //     Model = model;
+        //     Year = year;
+        //     Color = color;
+        // }
+
+        // 'static' means you do not need to instantiate a class before\
+        // using the class' method/property
+        public static void MyMethod()
+        {
+            Console.WriteLine("Called the static 'MyMethod'");
+        }
     }
 }
